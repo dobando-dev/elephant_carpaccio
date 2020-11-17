08a9e48fb2809f2dcac295942ba450ea5f42c9a4
Dobando <dobando@wearegap.com>
2020-11-17 16:39:57 +0000

    US1: Validar que se ingrese la cantidad

--- factura.rb
+++ factura.rb
@@ -2,6 +2,14 @@ class Factura
     def initialize()
         puts "Bienvenid@ al sistema de facturacion\n"
     end
+
+    def validar_input()
+        case ARGV.length
+        when 0
+            puts "Por favor ingrese una cantidad."
+        end
+    end
 end
 
 factura = Factura.new()
+factura.validar_input()
