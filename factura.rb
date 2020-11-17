8cd7f4b620ac1475cab825dd2db799b90851a205
Dobando <dobando@wearegap.com>
2020-11-17 20:22:54 +0000

    US-5: Verificar que se ingrese un precio unitario valido

--- factura.rb
+++ factura.rb
@@ -2,6 +2,7 @@ class Factura
     def initialize()
         puts "Bienvenid@ al sistema de facturacion\n"
         @cantidad = 0;
+        @precio_unitario = 0.0
     end
 
     def validar_input()
@@ -18,18 +19,28 @@ class Factura
             puts "Por favor ingrese el estado.\n"
         end
 
-        if(input_completo && validar_cantidad())
+        if(input_completo && validar_cantidad() && validar_precio_unitario())
 
         end
 
     end
 
     def validar_cantidad()
-        @cantidad = ARGV[0].to_f
+        @cantidad = ARGV[0].to_i
         if(@cantidad>0)
             return true
         end
         puts "Por favor ingrese una cantidad válida.\n"
+        return false
+    end
+
+    def validar_precio_unitario()
+        @precio_unitario = ARGV[1].to_f
+        puts @precio_unitario
+        if(@precio_unitario>0)
+            return true
+        end
+        puts "Por favor ingrese un precio unitario válido.\n"
         return false
     end
 end
