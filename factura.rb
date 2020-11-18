88fbc672679aa7151ec2439f4cac27c51b861363
Dobando <dobando@wearegap.com>
2020-11-18 01:42:16 +0000

    US-14: Aplicar descuento del 15% si el monto es mayor a 50000

--- factura.rb
+++ factura.rb
@@ -70,6 +70,7 @@ class Factura
         when 5000...7000 then porcentaje = 5
         when 7000...10000 then porcentaje = 7
         when 10000...50000 then porcentaje = 10
+        when 50000..then porcentaje = 15
         end
 
         if(porcentaje!=0)
